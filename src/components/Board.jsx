badf6147100eafc0ca8d3b1b3bc9016ad8be6185
Andrew <apwicker@gmail.com>
2024-05-04 08:30:59 -0700

    Create Board component for layout display
    Preset Layout arrays

---
+++ src/components/Board.jsx
@@ -0,0 +1,38 @@
+// import React from 'react';
+import { initializeBoard } from '../lib/utils/TilePlacementHelpers';
+
+export default function Board() {
+  const board = initializeBoard();
+
+  const tileUrls = {
+    Wainwright: '/tiles/01-wainwright.jpg',
+    'Fabric Warehouse': '/tiles/02-fabric-warehouse.jpg',
+    'Spice Warehouse': '/tiles/03-spice-warehouse.jpg',
+    'Fruit Warehouse': '/tiles/04-fruit-warehouse.jpg',
+    'Post Office': '/tiles/05-post-office.jpg',
+    Caravansary: '/tiles/06-caravansary.jpg',
+    Fountain: '/tiles/07-fountain.jpg',
+    'Black Market': '/tiles/08-black-market.jpg',
+    'Tea House': '/tiles/09-tea-house.jpg',
+    'Large Market': '/tiles/10-large-market.jpg',
+    'Small Market': '/tiles/11-small-market.jpg',
+    'Police Station': '/tiles/12-police-station.jpg',
+    "Sultan's Palace": '/tiles/13-sultans-palace.jpg',
+    'Small Mosque': '/tiles/14-small-mosque.jpg',
+    'Great Mosque': '/tiles/15-great-mosque.jpg',
+    'Gemstone Dealer': '/tiles/16-gemstone-dealer.jpg',
+  };
+
+  return (
+    <div className='grid grid-cols-4 border-[#333333] border-4'>
+      {board.map((tile, index) => (
+        <div
+          key={index}
+          className="bg-[url('tileUrls[tile]')] text-center"
+        >
+          <img src={tileUrls[tile]} />
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/lib/presetLayout.js
@@ -0,0 +1,53 @@
+export const shortPaths = [
+  [
+    { number: 15, name: 'Great Mosque' },
+    { number: 5, name: 'Post Office' },
+    { number: 2, name: 'Fabric Warehouse' },
+    { number: 14, name: 'Small Mosque' },
+  ],
+  [
+    { number: 4, name: 'Fruit Warehouse' },
+    { number: 12, name: 'Police Station' },
+    { number: 7, name: 'Fountain' },
+    { number: 3, name: 'Spice Warehouse' },
+  ],
+  [
+    { number: 8, name: 'Black Market' },
+    { number: 6, name: 'Caravansary' },
+    { number: 11, name: 'Large Market' },
+    { number: 9, name: 'Tea House' },
+  ],
+  [
+    { number: 13, name: "Sultan's Palace" },
+    { number: 10, name: 'Small Market' },
+    { number: 1, name: 'Wainwright' },
+    { number: 16, name: 'Gemstone Dealer' },
+  ],
+];
+
+export const longPaths = [
+  [
+    { number: 16, name: 'Gemstone Dealer' },
+    { number: 2, name: 'Fabric Warehouse' },
+    { number: 8, name: 'Black Market' },
+    { number: 11, name: 'Large Market' },
+  ],
+  [
+    { number: 15, name: 'Great Mosque' },
+    { number: 7, name: 'Fountain' },
+    { number: 6, name: 'Caravansary' },
+    { number: 4, name: 'Fruit Warehouse' },
+  ],
+  [
+    { number: 3, name: 'Spice Warehouse' },
+    { number: 5, name: 'Post Office' },
+    { number: 12, name: 'Police Station' },
+    { number: 1, name: 'Wainwright' },
+  ],
+  [
+    { number: 10, name: 'Small Market' },
+    { number: 9, name: 'Tea House' },
+    { number: 14, name: 'Small Mosque' },
+    { number: 13, name: "Sultan's Palace" },
+  ],
+];
