5086c6386858020f3e0db560a55bbe96a0aed6d3
Andrew <apwicker@gmail.com>
2024-05-06 20:03:46 -0700

    simple Footer component added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Board from './components/Board';
 import ButtonBar from './components/ButtonBar';
 import './App.css';
 import BlankBoard from './components/BlankBoard';
+import Footer from './components/Footer';
 
 function App() {
   const [boardType, setBoardType] = useState('blank');
@@ -51,7 +52,7 @@ function App() {
     <div id="istanbul">
       <div
         id="app"
-        className="flex bg-gradient-to-br from-slate-700 to-slate-300"
+        className="flex bg-gradient-to-br from-slate-700 to-black"
       >
         <div className="left-hand-panel flex flex-col items-center mt-4 w-1/5">
           <img
@@ -59,7 +60,7 @@ function App() {
             src="/images/istanbulTitle.png"
           />
           <h1 className="text-xxl font-bold text-center mb-8 text-[#ffffff] mt-8">
-            Tournament Layout App
+            Map Generator
           </h1>
           <ButtonBar
             onRegen={tournamentLayout}
@@ -69,6 +70,7 @@ function App() {
         </div>
         <div className="board-container flex w-4/5">{renderBoard()}</div>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className="flex justify-between">
+      <p className="mx-4">Copyright AEG, Pegasus Spiele</p>
+      <p>
+        This is a project built by a fan for fun and not in any way associated
+        with the designers, publishers, or distributors of the game.
+      </p>
+    </div>
+  );
+}
